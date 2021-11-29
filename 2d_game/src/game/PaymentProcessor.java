b4c4ebfcb77fda071aa3d953e567d82206ae3f8e
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2021-11-28 21:23:09 -0500

    Create PaymentProcessor.java

---
+++ 2d_game/src/game/PaymentProcessor.java
@@ -0,0 +1,13 @@
+package game;
+
+
+import java.sql.*;
+
+public class PaymentProcessor {
+  
+  static final String DBAddress = "database.address.com";
+  static final String User = "root";
+  static final String Password = "root";
+      
+
+}
